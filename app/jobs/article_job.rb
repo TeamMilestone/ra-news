e84e8ba3c10eede6665a49a8a55ace8ba2523759
jeff.dean <jeff.dean@kakaomobility.com>
2025-05-16 15:33:18 +0900

    상세 요약 작성 지침 수정 및 모델 설정 업데이트

--- app/jobs/article_job.rb
+++ app/jobs/article_job.rb
@@ -14,7 +14,7 @@ class ArticleJob < ApplicationJob
 주의 깊게 읽고 요약, 정리 한 내용을 한국어로 제공합니다.
 간단한 핵심 요약과 상세 요약을 제공합니다. 요약, 정리를 하기 위해 또 다른 사이트나 문서를 참고 할 수 있습니다.
 핵심 요약은 3줄 이내로 작성합니다.
-상세 요약은 서론(introduction)-본론(body)-결론(conclusion)의 3단 구조를 기본으로, 문단 간 연결어와 논리 전개 유지형 요약, OREO 기법, 피라미드 구조 및 전환 표현을 적극 활용합니다. 상세 요약은 500자 이상 1000자 이내로 작성합니다.
+상세 요약은 서론(introduction)-본론(body)-결론(conclusion)의 3단 구조를 기본으로 합니다. 상세 요약은 800자 이상 1600자 이내로 작성합니다.
 출력 결과는 JSON 형태로 제목(title_ko), 핵심 요약(summary_key), 상세 요약(summary_detail) 세 항목을 출력합니다.
 출력 예제
 {
@@ -26,10 +26,10 @@ class ArticleJob < ApplicationJob
   ],
   "summary_detail": { "introduction": "", "body": "", "conclusion": "" }
 }
-    PROMPT
+PROMPT
 
-    chat = RubyLLM.chat
-    chat.with_instructions("You are a Ruby programming lang and RubyOnRails framework expert.")
+    chat = RubyLLM.chat(model: "gemini-2.5-flash-preview-04-17", provider: :gemini, assume_model_exists: true)
+    chat.with_instructions("You are a Ruby programming lang and RubyOnRails framework expert. Be precise and concise. Use OREO technique, pyramid structure, and transition expressions actively.")
     response =  if article.is_youtube?
       # YouTube URL인 경우
       transcript = article.youtube_transcript
