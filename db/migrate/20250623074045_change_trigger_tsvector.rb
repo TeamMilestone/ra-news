ab0ace3ab21da4f835724512f440b28f6951c400
jeff.dean <jeff.dean@kakaomobility.com>
2025-06-23 16:48:21 +0900

    tsvector 컬럼에 한국어 설치

---
+++ db/migrate/20250623074045_change_trigger_tsvector.rb
@@ -0,0 +1,8 @@
+class ChangeTriggerTsvector < ActiveRecord::Migration[8.0]
+  def up
+    execute "CREATE EXTENSION IF NOT EXISTS textsearch_ko;" unless Rails.env.test?
+    create_trigger(compatibility: 1).on(:pg_search_documents).before(:insert, :update) do
+      "new.tsvector_content_tsearch := to_tsvector('pg_catalog.korean', coalesce(new.content,''));"
+    end
+  end
+end
