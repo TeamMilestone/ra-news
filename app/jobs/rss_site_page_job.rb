b9eb4bd35ce55ac3a8d274cd7f2ec312b4f53013
jeff.dean <jeff.dean@kakaomobility.com>
2025-07-17 17:57:49 +0900

    링크 처리 중 발생하는 오류를 로깅하도록 수정

--- app/jobs/rss_site_page_job.rb
+++ app/jobs/rss_site_page_job.rb
@@ -55,6 +55,8 @@ class RssSitePageJob < ApplicationJob
       next if Article.exists?(url: processed_link) || Article.exists?(origin_url: processed_link)
 
       create_article(origin_url: processed_link, url: processed_link, site: site)
+    rescue StandardError => e
+      logger.error "Error processing link #{link}: #{e.message}"
     end
 
     before_finish(ids)
