c5a3ab50d4c1db834c9d7fa5c0c0914dcea7638c
jeff.dean <jeff.dean@kakaomobility.com>
2025-07-23 22:44:51 +0900

    구식 도메인에서 새로운 도메인으로 리다이렉트하는 기능 추가

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :redirect_old_domain
+
+  private
+
+  def redirect_old_domain #: () -> void
+    if request.host == "news.stadiasphere.xyz"
+      redirect_to "https://ruby-news.kr#{request.fullpath}", status: 301, allow_other_host: true
+    end
+  end
 end
