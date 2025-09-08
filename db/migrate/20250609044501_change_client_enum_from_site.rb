6b70b773219d18830a53bc320ef5ac1bed290f62
jeff.dean <jeff.dean@kakaomobility.com>
2025-09-08 21:53:20 +0900

    re

--- db/migrate/20250609044501_change_client_enum_from_site.rb
+++ db/migrate/20250609044501_change_client_enum_from_site.rb
@@ -1,5 +1,5 @@
 class ChangeClientEnumFromSite < ActiveRecord::Migration[8.0]
   def change
-    change_column :sites, :client, :integer, using: 'client::integer', default: 0, null: false
+    change_column :sites, :client, :integer, using: 'client::integer', default: 0, null: false unless Rails.env.test?
   end
 end
