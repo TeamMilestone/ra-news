02937fd87f5c272894539fbb2073977370acb3f5
jeff.dean <jeff.dean@kakaomobility.com>
2025-09-08 22:08:09 +0900

    이제 될라나?

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-User.create!(email_address: "admin@example.com", password: "admin123")
+User.create!(email_address: "admin@example.com", password: "admin123", name: "Admin")
